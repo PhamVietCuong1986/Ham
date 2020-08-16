24317365570659051c26f8c377fddaec460228c7
Nguyen van D <vietcuong1226@gmail.com>
2020-08-16 21:47:17 +0700

    khai bao kieu du lieu cho gia tri ham tra ve

--- 1.js
+++ 1.js
@@ -1,3 +1,14 @@
 var x;
 x = 10;
 console.log(x);
+// funtion khai bao kieu du lieu cho gia tri  ham tra ve
+// ham tra ve gia tri kieu number
+function tinhtong() {
+    return 100 + 200;
+}
+console.log(tinhtong());
+// ham tra ve gia tri kieu string
+function hamstring() {
+    return "chao buoi sang";
+}
+console.log(hamstring());
--- 1.ts
+++ 1.ts
@@ -1,3 +1,15 @@
 var x : number;
 x = 10;
 console.log(x);
+// funtion khai bao kieu du lieu cho gia tri  ham tra ve
+// ham tra ve gia tri kieu number
+function tinhtong() :number{
+    return 100+200
+}
+console.log(tinhtong());
+// ham tra ve gia tri kieu string
+ function hamstring() : string {
+     return "chao buoi sang";
+ }
+ console.log(hamstring());
+ 
