b8b3f89621325ec33da5b274e1ca654ff13d9ebc
Nguyen van D <vietcuong1226@gmail.com>
2020-08-16 22:09:59 +0700

    khai bao gia tri tra ve ham dang doi tuong

--- 1.ts
+++ 1.ts
@@ -1,15 +1,46 @@
-var x : number;
-x = 10;
-console.log(x);
-// funtion khai bao kieu du lieu cho gia tri  ham tra ve
-// ham tra ve gia tri kieu number
-function tinhtong() :number{
-    return 100+200
+// var x : number;
+// x = 10;
+// console.log(x);
+// // funtion khai bao kieu du lieu cho gia tri  ham tra ve
+// // ham tra ve gia tri kieu number
+// function tinhtong() :number{
+//     return 100+200
+// }
+// console.log(tinhtong());
+// // ham tra ve gia tri kieu string
+//  function hamstring() : string {
+//      return "chao buoi sang";
+//  }
+//  console.log(hamstring());
+//  // ham tra ve mang
+//  function mang():string[]{
+//      return ["nam", "an","tiep"];
+//  }
+//  console.log(mang();
+//  )
+
+ // khai bao ham kieu doi tuong
+ // khai bao kieu truyen thong JS khong thuc thi duoc
+//   function game() : any{
+//       var nhanvatgame{
+//           ten: "chuot",
+//           kynang:{
+//               kynang1:"nhanh",
+//               kynang2:"tang hinh"
+//           }
+//       }
+//       return nhanvatgame;
+//   }
+//   console.log(game());
+  // khai bao ham kieu doi tuong
+ // khai bao kieu moi thi JS moi thuc thi duoc
+ function game() : any{
+      return { ten: "chuot",
+        kynang:{
+            kynang1:"nhanh",
+            kynang2:"tang hinh"
+        }
+    }
 }
-console.log(tinhtong());
-// ham tra ve gia tri kieu string
- function hamstring() : string {
-     return "chao buoi sang";
- }
- console.log(hamstring());
- 
+console.log(game());
+
