01e4b573ffa4b2c32cd8c11fdc8dafef62aea6e8
Nguyen van D <vietcuong1226@gmail.com>
2020-08-17 11:52:22 +0700

    khai niem ve lap trinh huong doi tuong

--- 2.ts
+++ 2.ts
@@ -8,4 +8,21 @@
 + Các đối tượng trao đổi với nhau thông qua các hàm. lúc này các hàm dduocj gọi là phương thức
 Bao gồm nhiều biến, tham số
 + Chương trình được thiết kế theo hướng tiếp cận từ dưới lên
++ Tái sử dụng code dễ dàng hơn
 */
+/* Các đặ điểm của lập trình hướng đối tượng:
+1.Trừu tượng hóa
+là việc xác định các thuộc tính, phương thức cần thiết cho đối tượng của 1 class
+Kết quả của trừu tượng hóa chính là bản thiết kế của 1 class
+2. Tính thừa kế: extend
+1 class có thể kế thừa các thuộc tính, phương thức của 1 class khác
+Nhằm tiết kiệm CODE và không gian bộ nhớ
+3.Tính đa hình: (abstract, interface)
+là kế thừa và viết lại các thuộc tính 1 cách chi tiết hơn, phương thức 1 cách chi tiết hơn của các lớp khác cho kết quả 1 lớp class mới
+thiết kế class (interface  dành cho property - thuộc tính
+                abstract dùng cho phương thức -method)
+4. Tính đóng gói (accessmodifier , statis)
+là tính chất cho phép hay không người dùng hay đối tượng khác thay đổi
+dữ liệu của đối tượng hiện tại
+
+
